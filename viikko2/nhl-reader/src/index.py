bf5d540df221b474bb9104adee9bf2f7245b6302
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-11-11 13:15:57 +0200

    teht 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,24 +1,40 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+
+        return players
+
+
+class PlayerStats:
+    def __init__(self, reader: PlayerReader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nat):
+        players = sorted(
+        [player for player in self.reader.get_players() if player.nationality == nat],
+        key = lambda x: x.points, reverse=True)
+
+        return players
+
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
 
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        players.append(player)
-
-    print("Oliot:")
-
-    players = sorted(
-        [player for player in players if player.nationality == "FIN"],
-        key = lambda x: x.points, reverse=True)
     for player in players:
         print(player)
 
