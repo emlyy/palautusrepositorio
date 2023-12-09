1460968fdb3fa440e4eb39b62a45792daf0692ac
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-12-09 14:56:23 +0200

    teht 2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,25 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not, All
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+
+    #matcher = And(
+        #Not(HasAtLeast(2, "goals")),
+        #HasFewerThan(20, "assists"),
+        #PlaysIn("NYR")
+    #)
+
+    #for player in stats.matches(matcher):
+        #print(player)
+
+
+if __name__ == "__main__":
+    main()
